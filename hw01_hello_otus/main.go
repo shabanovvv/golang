3b144a1f1132bea5819b56ca8ce9eb9e5cdb4424
Vyacheslav Shabanov <shabanovvv@hopper-it.ru>
2024-12-05 14:44:06 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	original := "Hello, OTUS!"
+	reversed := reverse.String(original)
+	fmt.Println(reversed)
 }
