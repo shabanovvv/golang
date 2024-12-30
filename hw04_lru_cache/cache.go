27987029cf22af62936882d93f847f8a85ffbfc5
Vyacheslav Shabanov <shabanovvv@hopper-it.ru>
2024-12-30 12:14:57 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,45 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	item, ok := c.items[key]
+	if ok {
+		item.Value = value
+		c.queue.MoveToFront(item)
+	} else {
+		item = c.queue.PushFront(value)
+		c.items[key] = item
+	}
+
+	if c.capacity < len(c.items) {
+		backItem := c.queue.Back()
+		if backItem != nil {
+			c.queue.Remove(backItem)
+			delete(c.items, key)
+		}
+	}
+
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+
+		return item.Value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem)
 }
 
 func NewCache(capacity int) Cache {
