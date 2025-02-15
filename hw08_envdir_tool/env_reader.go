b1d4c8199d5cfb19c589f1caab012811dccc31b4
Vyacheslav Shabanov <shabanovvv@hopper-it.ru>
2025-02-15 18:59:09 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strings"
+)
+
+var ErrNotExistEnvDir = errors.New("environment directory does not exist")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +20,47 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, ErrNotExistEnvDir
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			firstLine, err := GetFirstLine(dir + string(os.PathSeparator) + file.Name())
+			if err != nil {
+				return nil, err
+			}
+
+			env[file.Name()] = EnvValue{
+				strings.TrimRight(firstLine, " \t"),
+				len(firstLine) == 0,
+			}
+		}
+	}
+	return env, nil
+}
+
+func GetFirstLine(pathFile string) (string, error) {
+	file, err := os.Open(pathFile)
+	if err != nil {
+		return "", err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		line := scanner.Text()
+		line = strings.ReplaceAll(line, "\x00", "\n")
+		return line, nil
+	} else if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	return "", nil
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,67 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	tests := []struct {
+		name         string
+		cmd          []string
+		env          Environment
+		shouldFail   bool
+		expectedCode int
+	}{
+		{
+			name:         "Set variable and run command",
+			cmd:          []string{"env"},
+			env:          Environment{"TEST_VAR": {Value: "TEST_VALUE", NeedRemove: false}},
+			shouldFail:   false,
+			expectedCode: 0,
+		},
+		{
+			name:         "Unset variable and run command",
+			cmd:          []string{"env"},
+			env:          Environment{"TEST_VAR": {Value: "", NeedRemove: true}},
+			shouldFail:   false,
+			expectedCode: 0,
+		},
+		{
+			name:         "Run invalid command",
+			cmd:          []string{"invalid_command"},
+			env:          Environment{},
+			shouldFail:   true,
+			expectedCode: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Установка переменных окружения перед выполнением команды
+			returnCode := RunCmd(tt.cmd, tt.env)
+
+			if tt.shouldFail && returnCode == 0 {
+				t.Errorf("expected failure, got success")
+			}
+
+			if !tt.shouldFail && returnCode != 0 {
+				t.Errorf("expected success, got failure with code: %d", returnCode)
+			}
+
+			// Проверка значений переменных окружения
+			for k, v := range tt.env {
+				if v.NeedRemove {
+					if _, exists := os.LookupEnv(k); exists {
+						t.Errorf("expected environment variable %s to be unset", k)
+					}
+				} else {
+					val, exists := os.LookupEnv(k)
+					if !exists || val != v.Value {
+						t.Errorf("expected environment variable %s to be %s, got %s", k, v.Value, val)
+					}
+				}
+			}
+		})
+	}
 }
