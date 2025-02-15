e1b4c0e6ea95c5d4956fbaa7ee77fd8feac38ef5
Vyacheslav Shabanov <shabanovvv@hopper-it.ru>
2025-02-15 18:59:09 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("directory does not exist", func(t *testing.T) {
+		_, err := ReadDir("./321123312/")
+		require.Truef(t, errors.Is(err, ErrNotExistEnvDir), "%v", err)
+	})
+
+	t.Run("directory exists and check env", func(t *testing.T) {
+		env, err := ReadDir("./testdata/env")
+		require.NoError(t, err)
+		require.Len(t, env, 5)
+
+		expectedEnv := Environment{
+			"BAR":   {Value: "bar", NeedRemove: false},
+			"EMPTY": {Value: "", NeedRemove: false},
+			"FOO":   {Value: "   foo\nwith new line", NeedRemove: false},
+			"HELLO": {Value: "\"hello\"", NeedRemove: false},
+			"UNSET": {Value: "", NeedRemove: true},
+		}
+		require.Equal(t, expectedEnv, env)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go-envdir /path/to/env/dir command arg1 arg2 ...")
+		os.Exit(1)
+	}
+
+	envDir := os.Args[1]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Println("Error reading environment directory:", err)
+		os.Exit(1)
+	}
+
+	// Передаем команду и её аргументы
+	command := os.Args[2:]
+	exitCode := RunCmd(command, env)
+
+	os.Exit(exitCode)
 }
