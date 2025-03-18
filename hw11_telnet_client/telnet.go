d6a7974223c5574845d0e4add100e6f5a4d686bc
Vyacheslav Shabanov <shabanovvv@hopper-it.ru>
2025-03-18 08:27:26 +0300

    HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -12,10 +18,94 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type telnetClient struct {
+	ctx     context.Context
+	conn    net.Conn
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	cancel  context.CancelFunc
+}
+
+func (t *telnetClient) Connect() error {
+	dialer := &net.Dialer{}
+	conn, err := dialer.DialContext(t.ctx, "tcp", t.address)
+	if err != nil {
+		return err
+	}
+	t.conn = conn
+	_, err = fmt.Fprintf(os.Stderr, "...Connected to %s\n", t.address)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *telnetClient) Send() error {
+	buf := make([]byte, 1024)
+	n, err := t.in.Read(buf)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+	if n > 0 {
+		_, err = t.conn.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *telnetClient) Receive() error {
+	scanner := bufio.NewScanner(t.conn)
+	for scanner.Scan() {
+		select {
+		case <-t.ctx.Done():
+			return t.Close()
+		default:
+			_, err := t.out.Write(scanner.Bytes())
+			if err != nil {
+				return err
+			}
+
+			_, err = t.out.Write([]byte{'\n'})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
+	return nil
+}
+
+func (t *telnetClient) Close() error {
+	defer t.cancel()
+	if t.conn != nil {
+		err := t.conn.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+
+	return &telnetClient{
+		ctx:     ctx,
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+		cancel:  cancel,
+	}
+}
